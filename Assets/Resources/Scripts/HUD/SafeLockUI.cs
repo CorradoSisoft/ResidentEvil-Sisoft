f79ea3a12aeff5eab6c89c0bfe752ac047636724
corryws <corradotrigilia97@gmail.com>
2026-03-13 08:25:08 +0100

    Continua alla Sisoft con Bug Fix - Riempimento Documenti Speciali - Cambiare "Grafica" agli oggetti prendibili

--- Assets/Resources/Scripts/HUD/SafeLockUI.cs
+++ Assets/Resources/Scripts/HUD/SafeLockUI.cs
@@ -3,15 +3,41 @@ using UnityEngine.UI;
 
 public class SafeLockUI : MonoBehaviour
 {
-    public Button[] numberButtons; // 0-9
-    
+    public Button[] numberButtons;
+    public Canvas targetCanvas; // ← trascina il MainCanvas qui
+
+    private Camera canvasCamera;
+
     void Start()
     {
-        for (int i = 0; i < numberButtons.Length; i++)
+        canvasCamera = targetCanvas != null ? targetCanvas.worldCamera : null;
+        Debug.Log($"Camera: {canvasCamera}");
+
+        /* for (int i = 0; i < numberButtons.Length; i++)
         {
             int captured = i;
-            numberButtons[i].onClick.AddListener(() => 
+            numberButtons[i].onClick.AddListener(() =>
                 SafeLock.Current?.PressKey(captured.ToString()));
+        } */
+    }
+
+    void Update()
+    {
+        if (!SafeLock.IsAnyOpen) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            for (int i = 0; i < numberButtons.Length; i++)
+            {
+                if (numberButtons[i] == null) continue;
+                int captured = i;
+                if (RectTransformUtility.RectangleContainsScreenPoint(
+                    numberButtons[i].GetComponent<RectTransform>(), Input.mousePosition, canvasCamera))
+                {
+                    SafeLock.Current?.PressKey(captured.ToString());
+                    return;
+                }
+            }
         }
     }
 }
